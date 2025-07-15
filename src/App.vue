3787d303d86e2266b59c32775cdd4dc83af1ec74
stakic-igor <stakic_igor@yahoo.com>
2025-07-15 21:38:03 +0200

    add basic single task view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+  import SingleTask from './components/SingleTask.vue';
 
 </script>
 
 <template>
- 
+ <h1>Welcome to My Todo List</h1>
+ <div v-for="i in 5" :key="i">
+   <SingleTask />
+ </div>
 </template>
 
 <style scoped>
---
+++ src/components/SingleTask.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+    <div>
+        <p>15. 7. 2025</p>
+        <button>delete</button>
+    </div>
+    <h3>single task -taskID</h3>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Ipsam, quibusdam.
+    </p>
+    <button>Done!</button>
+</template>
