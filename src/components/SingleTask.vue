ed38ea56dc01f4cd35938a5963182e7befae352d
Igor Stakic <stakic_igor@yahoo.com>
2025-07-16 20:31:10 +0200

    add test styles for single task

--- src/components/SingleTask.vue
+++ src/components/SingleTask.vue
@@ -12,11 +12,11 @@ const props = defineProps({
 </script>
 
 <template>
-    <div>
-        <p>{{ props.date }}</p>
+    <div class="flex flex-row">
+        <p class="text-sm text-gray-500">{{ props.date }}</p>
         <Button button-text="Delete" />
     </div>
-    <h3>{{ props.title }} - {{ props.id }}</h3>
+    <h3 class="text-xl">{{ props.title }} - {{ props.id }}</h3>
     <p>{{ props.description }}</p>
     <p>status: {{ props.isCompleted }}</p>
     <Button button-text="Mark as done" />
